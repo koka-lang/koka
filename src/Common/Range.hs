a1996d930384f9f5efc4ef627a258fa985081723
daan <none@none>
2016-06-09 10:07:26 -0700

    Use internal utf8 decoding
    --HG--
    branch : algeff-dev

--- src/Common/Range.hs
+++ src/Common/Range.hs
@@ -35,7 +35,7 @@ import Common.Failure( assertion )
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Text as T (Text, pack, unpack)
-import qualified Data.Text.Encoding as T (decodeUtf8) --, decodeUtf8With)
+import qualified Data.Text.Encoding as T (decodeUtf8) -- ,decodeUtf8With)
 -- import qualified Data.Text.Encoding.Error as E(lenientDecode)
 -- import Common.Name(showHex)
 
@@ -48,7 +48,7 @@ bstringEmpty = B.empty
 
 bstringToText bstr = T.pack (BC.unpack bstr) -- utfDecode bstr -- T.decodeUtf8With E.lenientDecode bstr  
 
-bstringToString bstr = T.unpack (utfDecode bstr) -- (bstringToText bstr)
+bstringToString bstr = T.unpack (T.decodeUtf8 bstr) -- (bstringToText bstr)
 
 stringToBString str = BC.pack str
 
