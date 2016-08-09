dd02ec77bb4e461933cbbef4003813c0d1e9014c
Daan <none@none>
2016-08-08 17:27:34 -0700

    Update gneerated lexer
    --HG--
    branch : algeff-dev

--- src/Syntax/Lexer.hs
+++ src/Syntax/Lexer.hs
@@ -357,26 +357,6 @@ alex_action_48 =  string $ \s -> LexError ("illegal character in line directive:
 {-# LINE 1 "templates\\GenericTemplate.hs" #-}
 {-# LINE 1 "<built-in>" #-}
 {-# LINE 1 "<command-line>" #-}
-{-# LINE 9 "<command-line>" #-}
-{-# LINE 1 "G:\\GitHub\\haskell-platform\\build\\ghc-bindist\\local\\lib/include\\ghcversion.h" #-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{-# LINE 9 "<command-line>" #-}
 {-# LINE 1 "templates\\GenericTemplate.hs" #-}
 -- -----------------------------------------------------------------------------
 -- ALEX TEMPLATE
@@ -387,19 +367,19 @@ alex_action_48 =  string $ \s -> LexError ("illegal character in line directive:
 -- -----------------------------------------------------------------------------
 -- INTERNALS and main scanner engine
 
-{-# LINE 21 "templates\\GenericTemplate.hs" #-}
+{-# LINE 35 "templates\\GenericTemplate.hs" #-}
 
-{-# LINE 51 "templates\\GenericTemplate.hs" #-}
+{-# LINE 45 "templates\\GenericTemplate.hs" #-}
 
-{-# LINE 72 "templates\\GenericTemplate.hs" #-}
+{-# LINE 66 "templates\\GenericTemplate.hs" #-}
 alexIndexInt16OffAddr arr off = arr ! off
 
 
-{-# LINE 93 "templates\\GenericTemplate.hs" #-}
+{-# LINE 87 "templates\\GenericTemplate.hs" #-}
 alexIndexInt32OffAddr arr off = arr ! off
 
 
-{-# LINE 105 "templates\\GenericTemplate.hs" #-}
+{-# LINE 98 "templates\\GenericTemplate.hs" #-}
 quickIndex arr i = arr ! i
 
 
