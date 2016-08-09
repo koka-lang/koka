9a8749f1af5aa03742e79fde6457cfd225aec45d
daan <none@none>
2016-08-08 17:03:02 -0700

    Update lexer generated file
    --HG--
    branch : algeff-dev

--- src/Syntax/Lexer.hs
+++ src/Syntax/Lexer.hs
@@ -357,7 +357,6 @@ alex_action_48 =  string $ \s -> LexError ("illegal character in line directive:
 {-# LINE 1 "templates\\GenericTemplate.hs" #-}
 {-# LINE 1 "<built-in>" #-}
 {-# LINE 1 "<command-line>" #-}
-<<<<<<< working copy
 {-# LINE 9 "<command-line>" #-}
 {-# LINE 1 "G:\\GitHub\\haskell-platform\\build\\ghc-bindist\\local\\lib/include\\ghcversion.h" #-}
 
@@ -378,10 +377,6 @@ alex_action_48 =  string $ \s -> LexError ("illegal character in line directive:
 
 
 {-# LINE 9 "<command-line>" #-}
-=======
->>>>>>> destination
-
-{-# LINE 6 "<command-line>" #-}
 {-# LINE 1 "templates\\GenericTemplate.hs" #-}
 -- -----------------------------------------------------------------------------
 -- ALEX TEMPLATE
