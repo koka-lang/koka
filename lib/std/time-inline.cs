00798e06b4c81fcce6495b5542d08421d85e5806
daan <none@none>
2016-09-27 22:37:34 -0700

    More updates to std/time. Durations in seconds, week-of-year, and parse-imf
    --HG--
    branch : algeff-dev

--- lib/std/time-inline.cs
+++ lib/std/time-inline.cs
@@ -14,29 +14,36 @@ static class _Time
   static TimeZone  local  = TimeZone.CurrentTimeZone; // ensure it stays the same
 
   public static int TimezoneOffset( DateTime d ) {
-    return (d.Kind == DateTimeKind.Utc ? 0 : Convert.ToInt32(local.GetUtcOffset(d).TotalMilliseconds));
+    return (d.Kind == DateTimeKind.Utc ? 0 : Convert.ToInt32(local.GetUtcOffset(d).TotalSeconds));
   }
 
-  public static double EpochMsecs( DateTime d ) {
-    return (d.ToUniversalTime() - epoch).TotalMilliseconds;
+  public static double EpochSecs( DateTime d ) {
+    return (d.ToUniversalTime() - epoch).TotalSeconds;
+  }
+
+  public static int DayOfWeek( DateTime d ) {
+    int w = (int)d.DayOfWeek;
+    return (w==0 ? 7 : w);
   }
 
   public static DateTime New( int year, int month, int day, int hours, int minutes, int seconds, int milliseconds, bool isUtc ) {
     return new DateTime(year,month,day,hours,minutes,seconds,milliseconds, isUtc ? DateTimeKind.Utc : DateTimeKind.Local );
   }
 
-  public static DateTime NewFromEpoch( double msecs, bool isUtc ) {
-    long eticks = Convert.ToInt64(msecs * 10000.0); // to 100-nanoseconds
-    return new DateTime( epoch.Ticks + eticks, isUtc ? DateTimeKind.Utc : DateTimeKind.Local ); 
+  public static DateTime NewFromEpoch( double secs, bool isUtc ) {
+    long eticks = Convert.ToInt64(secs * 1.0e7); // to 100-nanoseconds
+    DateTime d = new DateTime( epoch.Ticks + eticks, DateTimeKind.Utc); 
+    return (isUtc ? d : d.ToLocalTime());
   }
 
-  public static std_core._Tuple3_<int,int,int> Date( DateTime d ) {
-    return new std_core._Tuple3_<int,int,int>(d.Year, d.Month, d.Day);
-  }
+  public static int Year( DateTime d )    { return d.Year; }
+  public static int Month( DateTime d )   { return d.Month; }
+  public static int Day( DateTime d )     { return d.Day; }
+  public static int Hours( DateTime d )   { return d.Hour; }
+  public static int Minutes( DateTime d ) { return d.Minute; }
+  public static int Seconds( DateTime d ) { return d.Second; }
+  public static int Milliseconds( DateTime d ) { return d.Millisecond; }
 
-  public static std_core._Tuple4_<int,int,int,int> Clock( DateTime d ) {
-    return new std_core._Tuple4_<int,int,int,int>(d.Hour,d.Minute,d.Second,d.Millisecond);
-  }
 
   // Ticks
   static Stopwatch sw = Stopwatch.StartNew();
@@ -45,7 +52,7 @@ static class _Time
     long   fticks = sw.ElapsedTicks;
     double secs = Convert.ToDouble(fticks/Stopwatch.Frequency) +
                   Convert.ToDouble(fticks%Stopwatch.Frequency)*TicksResolution;
-    return (secs * 1000.0);                  
+    return secs;                  
   }
 
   public static double TicksResolution = 1.0 / Convert.ToDouble(Stopwatch.Frequency); 
