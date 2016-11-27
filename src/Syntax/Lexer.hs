d13979909cdd2e18a7e3165bfdbd7a43e230d0d0
daan <none@none>
2016-11-27 11:41:16 -0800

    Allow a dash after a digit in an identifier
    --HG--
    branch : bigint

--- src/Syntax/Lexer.hs
+++ src/Syntax/Lexer.hs
@@ -146,7 +146,7 @@ isMalformed :: String -> Bool
 isMalformed s
   = case s of
       '-':c:cs   | not (isLetter c) -> True
-      c:'-':cs   | not (isLetter c) -> True
+      c:'-':cs   | not (isLetter c || isDigit c) -> True
       c:cs       -> isMalformed cs
       []         -> False
 
