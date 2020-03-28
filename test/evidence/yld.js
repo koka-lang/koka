24a12381bbec8073a7eb3361615b830a9668eb31
daan <daanl@outlook.com>
2020-03-28 14:05:13 -0700

    cleanup

--- test/evidence/yld.js
+++ test/evidence/yld.js
@@ -6,11 +6,15 @@
   found in the file "license.txt" at the root of this distribution.
 ---------------------------------------------------------------------------*/
 
-const _context = { yield: 0, evv: [], yield_conts : [], yield_clause : null };
+var $evv = [];
+var $yield = null; // { marker: 0, clause: null, conts: [], conts_count: 0 };
+var $marker_unique = 1;
 
-var _marker_unique = 1;
+//--------------------------------------------------
+// evidence
+//--------------------------------------------------
 
-function evv_insert_ev(evv,ev) {
+function _evv_insert(evv,ev) {
   const n = evv.length;
   const evv2 = new Array(n+1);
   var i;
@@ -23,11 +27,10 @@ function evv_insert_ev(evv,ev) {
   for(; i < n; i++) {
     evv2[i+1] = evv[i];
   }
-  _context.evv = evv2;
   return evv2;
 }
 
-function evv_lookup(evv,tag) {
+function _evv_lookup(evv,tag) {
   for(var i = 0; i < evv.length; i++) {
     if (tag === evv[i]._field1) return evv[i];
   }
@@ -35,62 +38,60 @@ function evv_lookup(evv,tag) {
   return null;
 }
 
-function _kcompose( start, conts ) {
+//--------------------------------------------------
+// Yielding
+//--------------------------------------------------
+function _yielding() {
+  return ($yield !== null);
+}
+
+function _kcompose( from, to, conts ) {
   return function(x) {
-    var y = x;
-    for(var i = start; i < conts.length; i++) {
-      y = conts[i](y);
-      if (_context.yield !== 0) {
-        return (function(i){ return _yield_extend(_kcompose(i+1,conts)); })(i);
-      }
+    var acc = x;
+    for(var i = from; i < to; i++) {
+      acc = conts[i](acc);
+      if (_yielding()) return ((function(i){ return _yield_extend(_kcompose(i+1,to,conts)); })(i));
     }
-    return y;
+    return acc;
   }
 }
 
 function _yield_extend(next) {
-  if (_context.yield===0) console.error("yield extension while not yielding!");
-  _context.yield_conts.push(next);
-  //const cont = _context.yield_cont;
-  //_context.yield_cont = kliesli_compose(cont,next);
+  if (!_yielding()) console.error("yield extension while not yielding!");
+  $yield.conts[$yield.conts_count++] = next;  // index is ~80% faster as push
   return undefined;
 }
 
 function _yield_cont(f) {
-  if (_context.yield===0) console.error("yield extension while not yielding!");
-  const conts = _context.yield_conts;
-  _context.yield_conts = new Array();
-  _context.yield_conts.push( function(x){ return f(_kcompose(0,conts),x); } );
+  if (!_yielding()) console.error("yield extension while not yielding!");
+  const cont   = _kcompose(0,$yield.conts_count,$yield.conts);
+  $yield.conts = new Array(8);
+  $yield.conts_count = 1;
+  $yield.conts[0] = function(x){ return f(cont,x); };
   return undefined;
 }
 
 function _yield_prompt(evv, evv_expected, m, res) {
-  if (_context.evv !== evv_expected && (_context.yield === 0 || _context.yield === m)) console.log("non-matching evidence: " + JSON.stringify(_context,null,1) + "\n  expecting: " + JSON.stringify(evv_expected,null,1));
-  if (_context.yield === 0) {
-    _context.evv = evv;
+  if ($evv !== evv_expected && ($yield===null || $yield.marker === m)) console.log("non-matching evidence: " + JSON.stringify(_context,null,1) + "\n  expecting: " + JSON.stringify(evv_expected,null,1));
+  $evv = evv;
+  if ($yield === null) {
     return Pure(res);
   }
-  else if (_context.yield !== m) {
-    _context.evv = evv;
+  else if ($yield.marker !== m) {
     return Yielding;
   }
   else {
-    _context.evv = evv;
-    const conts = _context.yield_conts;
-    const clause = _context.yield_clause;
-    _context.yield = 0;
-    _context.yield_conts = null;
-    _context.yield_clause = null;
-    return Yielded(clause, _kcompose(0,conts));
+    const cont   = _kcompose(0,$yield.conts_count,$yield.conts);
+    const clause = $yield.clause;
+    $yield = null;
+    return Yielded(clause,cont);
   }
 }
 
 function _yield_to(m,clause) {
-  if (_context.yield!==0) console.error("yielding while yielding!");
-  const evv = _context.evv;
-  _context.yield = m;
-  _context.yield_clause = clause;
-  _context.yield_conts = new Array();
-  _context.yield_conts.push(function(x){ _context.evv = evv; return x; });
+  if (_yielding()) console.error("yielding while yielding!");
+  const evv = $evv;
+  $yield = { marker: m, clause: clause, conts: new Array(8), conts_count: 1 };
+  $yield.conts[0] = function(x){ $evv = evv; return x; };
   return undefined;
 }
