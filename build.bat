9bdb335ba8abcc503aa275c73b08f009764d8d26
Alex Reinking <alex.reinking@gmail.com>
2020-08-01 10:39:40 -0700

    Make build.bat more verbose

--- build.bat
+++ build.bat
@@ -7,8 +7,8 @@ cmake --build cruntime/out/msvc-x64/Debug --config Debug
 
 stack build
 
-stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core/types >nul
-stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core >nul
+stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core/types
+stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core
 
 echo Run with: stack exec koka -- --target=c --core --checkcore -ilib -itest/algeff -itest/cgen -itest/parc -itest/lib
 echo To test PARC, run: stack test --test-arguments="--match /parc/"
