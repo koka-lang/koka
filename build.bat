c86d164a11b59c3a31b65b879ff26797f0eceaca
Alex Reinking <alex.reinking@gmail.com>
2020-08-01 15:09:32 -0700

    Generate CMake packaging. Update build.bat.

--- build.bat
+++ build.bat
@@ -2,10 +2,19 @@
 
 pushd %~dp0
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -S cruntime -B cruntime/out/msvc-x64/Debug
-cmake --build cruntime/out/msvc-x64/Debug --config Debug
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=cruntime/out/install -S cruntime -B cruntime/out/Debug
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=cruntime/out/install -S cruntime -B cruntime/out/Release
+
+cmake --build cruntime/out/Debug --config Debug
+cmake --build cruntime/out/Release --config Debug
+
+cmake --install cruntime/out/Debug
+cmake --install cruntime/out/Release
 
 stack build
+
+stack exec koka -- --target=c -c -ilib -l std/core/types
+stack exec koka -- --target=c -c -ilib -l std/core
 
 stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core/types
 stack exec koka -- --target=c --outdir=out/test -c -ilib -l std/core
