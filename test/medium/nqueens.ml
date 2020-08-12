95543ca13be335070457e3f7ce7f67fc92c5cddd
daan <daanl@outlook.com>
2020-08-11 22:27:01 -0700

    add nqueens.ml

---
+++ test/medium/nqueens.ml
@@ -0,0 +1,23 @@
+open List;;
+
+let rec safe queen diag  = fun
+  | q :: qs ->
+     queen <> q && queen <> q + diag && queen <> q - diag && safe queen (diag + 1) qs;;
+  | [] -> true
+
+let rec append_safe queen xs xss =
+  if (queen <= 0) then xss
+  else if (safe queen 1 xs) then append_safe (queen - 1) xs ((queen :: xs) :: xss)
+  else append_safe (queen - 1) xs xss 
+
+let rec extend queen acc = fun
+  | xs :: rest -> extend queen (append_safe queen xs acc) rest
+  | [] -> acc
+
+let rec find_solutions n queen =
+  if (queen == 0) then [[]]
+   else extend n [] (find_solutions n (queen - 1))
+   
+let queens n = List.length (find_solutions n n)
+
+Printf.printf "%8d\n" (queens 13);
