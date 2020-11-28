6a8d3037f2960d498835bae3d3682f9a4cf93c14
unknown <daanl@outlook.com>
2020-11-27 23:16:39 -0800

    test install redirect

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 
 <head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<meta http-equiv="refresh" content="0;URL='https://github.com/koka-lang/koka/releases/download/v2.0.9/install.sh'" />
 
-<title>Koka Install Redirect</title>
+<title>301 Redirect</title>
 </head>
 
 <body>
 
-See <a href="https://github.com/koka-lang/koka/releases/download/v2.0.9/install.sh">
-https://github.com/koka-lang/koka/releases/download/v2.0.9/install.sh</a> for the latest install script.
+<a href="https://github.com/koka-lang/koka/releases/download/v2.0.9/install.sh">redirect</a>
+
 </body>
