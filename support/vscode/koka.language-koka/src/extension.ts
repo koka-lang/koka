fc98530e2a2f7d5d6f2acad1c388eb7524db1b5a
fwcd <fwcdmail@gmail.com>
2020-12-08 17:12:34 +0100

    Only start language client in VSCode extension if option is set

--- support/vscode/koka.language-koka/src/extension.ts
+++ support/vscode/koka.language-koka/src/extension.ts
@@ -3,24 +3,26 @@ import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOpt
 
 export function activate(context: vscode.ExtensionContext) {
     const config = vscode.workspace.getConfiguration('koka');
-    const path = config.get('executablePath') as string;
-    
-    const serverOptions: ServerOptions = {
-        command: path,
-        args: ["--language-server"]
-    };
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            { language: 'koka', scheme: 'file' }
-        ],
-        outputChannelName: 'Koka',
-        revealOutputChannelOn: RevealOutputChannelOn.Never
-    };
-    const client = new LanguageClient(
-        'Koka Language Client',
-        serverOptions,
-        clientOptions
-    );
 
-    context.subscriptions.push(client.start());
+    if (config.get('languageServer.enabled')) {
+        const executablePath = config.get('languageServer.path') as string;
+        const serverOptions: ServerOptions = {
+            command: executablePath,
+            args: ["--language-server"]
+        };
+        const clientOptions: LanguageClientOptions = {
+            documentSelector: [
+                { language: 'koka', scheme: 'file' }
+            ],
+            outputChannelName: 'Koka',
+            revealOutputChannelOn: RevealOutputChannelOn.Never
+        };
+        const client = new LanguageClient(
+            'Koka Language Client',
+            serverOptions,
+            clientOptions
+        );
+
+        context.subscriptions.push(client.start());
+    }
 }
