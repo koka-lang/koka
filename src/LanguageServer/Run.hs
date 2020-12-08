ed510ef5744adcfc739efa8c14382fb64752c932
fwcd <fwcdmail@gmail.com>
2020-12-08 12:58:27 +0100

    Add missing options to language server runner

--- src/LanguageServer/Run.hs
+++ src/LanguageServer/Run.hs
@@ -1,8 +1,9 @@
 module LanguageServer.Run( runLanguageServer
                          ) where
 
-import Compiler.Options ( Flags )
-import Control.Monad    ( void )
+import Compiler.Options          ( Flags )
+import Control.Monad             ( void )
+import Control.Monad.IO.Class    ( liftIO )
 import Language.LSP.Server
 import Language.LSP.Types
 import LanguageServer.Handlers
@@ -12,4 +13,6 @@ runLanguageServer flags files = void $ runServer $ ServerDefinition
   { onConfigurationChange = const $ pure $ Right ()
   , doInitialize = \env _ -> pure $ Right env
   , staticHandlers = handlers
+  , interpretHandler = \env -> Iso (runLspT env) liftIO
+  , options = defaultOptions
   }
