ae48beb46f6e835d4d67ad83b7415889a2131d03
fwcd <fwcdmail@gmail.com>
2020-12-10 05:48:20 +0100

    Enable open/close notifications

--- src/LanguageServer/Run.hs
+++ src/LanguageServer/Run.hs
@@ -4,12 +4,13 @@
 module LanguageServer.Run( runLanguageServer
                          ) where
 
-import Compiler.Options          ( Flags )
-import Control.Monad             ( void )
-import Control.Monad.IO.Class    ( liftIO )
+import Compiler.Options                  ( Flags )
+import Control.Monad                     ( void )
+import Control.Monad.IO.Class            ( liftIO )
 import Language.LSP.Server
+import qualified Language.LSP.Types      as J
 import LanguageServer.Handlers
-import LanguageServer.Monad      ( runLSM )
+import LanguageServer.Monad              ( runLSM )
 
 runLanguageServer :: Flags -> [FilePath] -> IO ()
 runLanguageServer flags files = void $ runServer $ ServerDefinition
@@ -18,4 +19,12 @@ runLanguageServer flags files = void $ runServer $ ServerDefinition
   , staticHandlers = handlers flags
   , interpretHandler = \env -> Iso (`runLSM` env) liftIO
   , options = defaultOptions
+    { textDocumentSync = Just syncOptions
+    }
   }
+  where syncOptions = J.TextDocumentSyncOptions
+                        (Just True) -- open/close notifications
+                        (Just J.TdSyncIncremental) -- changes
+                        (Just False) -- will save
+                        (Just False) -- will save (wait until requests are sent to server)
+                        (Just $ J.InR $ J.SaveOptions $ Just False) -- save
