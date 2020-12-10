14366857f6659b1b873e3c04ecb8f0e8b10f0db3
fwcd <fwcdmail@gmail.com>
2020-12-10 03:24:18 +0100

    Add LSP initialization handler

---
+++ src/LanguageServer/Handler/Initialized.hs
@@ -0,0 +1,14 @@
+-----------------------------------------------------------------------------
+-- The LSP handlers that handles initialization
+-----------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+module LanguageServer.Handler.Initialized( initializedHandler
+                                         ) where
+
+import Compiler.Options                  ( Flags )
+import Language.LSP.Server               ( notificationHandler, sendNotification, Handlers, LspM )
+import qualified Language.LSP.Types      as J
+
+initializedHandler :: Flags -> Handlers (LspM ())
+initializedHandler flags = notificationHandler J.SInitialized $ \_not -> do
+    sendNotification J.SWindowLogMessage $ J.LogMessageParams J.MtInfo "Initialized language server."
--- src/LanguageServer/Handlers.hs
+++ src/LanguageServer/Handlers.hs
@@ -7,13 +7,15 @@ module LanguageServer.Handlers( handlers
 import Compiler.Options                    ( Flags )
 import Language.LSP.Server
 import LanguageServer.Handler.Hover        ( hoverHandler )
+import LanguageServer.Handler.Initialized  ( initializedHandler )
 import LanguageServer.Handler.TextDocument ( didOpenHandler, didChangeHandler, didSaveHandler, didCloseHandler )
 
 handlers :: Flags -> Handlers (LspM ())
 handlers flags = mconcat
-  [ hoverHandler flags
+  [ initializedHandler flags
   , didOpenHandler flags
   , didChangeHandler flags
   , didSaveHandler flags
   , didCloseHandler flags
+  , hoverHandler flags
   ]
