9381e7091719569a76e63862058f7028fd8ac363
fwcd <fwcdmail@gmail.com>
2020-12-11 19:39:30 +0100

    Traverse expressions and patterns for symbols

--- src/LanguageServer/Handler/DocumentSymbol.hs
+++ src/LanguageServer/Handler/DocumentSymbol.hs
@@ -7,13 +7,14 @@ module LanguageServer.Handler.DocumentSymbol( documentSymbolHandler
                                             ) where
 
 import qualified Common.Range            as R
+import Common.Syntax                     ( DefSort (..) )
 import Common.Name                       ( Name (..) )
 import Compiler.Options                  ( Flags )
 import Compiler.Module                   ( modProgram, loadedModule, Loaded (..) )
 import Control.Lens                      ( (^.) )
 import qualified Data.Map                as M
-import Data.Maybe                        ( maybeToList )
-import Data.Text                         as T
+import Data.Maybe                        ( catMaybes,  maybeToList )
+import qualified Data.Text               as T
 import Language.LSP.Server               ( requestHandler, Handlers )
 import qualified Language.LSP.Types      as J
 import qualified Language.LSP.Types.Lens as J
@@ -48,18 +49,49 @@ instance HasSymbols UserDefGroup where
     DefNonRec d  -> symbols d
 
 instance HasSymbols UserDef where
-  symbols d = symbols $ defBinder d
-
-instance HasSymbols (ValueBinder () UserExpr) where
-  symbols b = [makeSymbol n J.SkFunction r cs]
+  symbols d = [makeSymbol n k r cs]
     where
+      b = defBinder d
+      k = case defSort d of
+        DefFun -> J.SkFunction
+        DefVal -> J.SkConstant
+        DefVar -> J.SkVariable
       n = binderName b
       r = binderRange b
       cs = symbols $ binderExpr b
 
 instance HasSymbols UserExpr where
-  -- TODO
-  symbols e = []
+  symbols ex = case ex of
+    Let dg e _     -> symbols dg ++ symbols e
+    Bind d e _     -> symbols d ++ symbols e
+    App e nes _    -> symbols e ++ (symbols . snd =<< nes)
+    Ann e _ _      -> symbols e
+    Case e bs _    -> symbols e ++ (symbols =<< bs)
+    Parens e _ _   -> symbols e
+    Inject _ e _ _ -> symbols e
+    _              -> [] -- TODO: Handle other types of (nested) expressions
+
+instance HasSymbols UserBranch where
+  symbols b = symbols p ++ (symbols =<< gs)
+    where
+      p = branchPattern b
+      gs = branchGuards b
+
+instance HasSymbols UserGuard where
+  symbols g = symbols t ++ symbols e
+    where
+      t = guardTest g
+      e = guardExpr g
+
+instance HasSymbols UserPattern where
+  symbols pat = case pat of
+    PatVar b        -> let n = binderName b
+                           r = binderRange b
+                       in [makeSymbol n J.SkConstant r []]
+    PatAnn p _ _    -> symbols p
+    PatCon _ ps _ _ -> symbols . snd =<< ps
+    PatParens p _   -> symbols p
+    _ -> []
 
 makeSymbol :: Name -> J.SymbolKind -> R.Range -> [J.DocumentSymbol] -> J.DocumentSymbol
 makeSymbol n k r cs = J.DocumentSymbol name detail kind deprecated range selRange children
