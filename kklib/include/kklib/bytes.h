bc2db90e446e2a3e82db22a20b764c97cfe4fe95
Daan Leijen <daan@microsoft.com>
2021-06-14 23:13:31 -0700

    fix few more copyright notices

--- kklib/include/kklib/bytes.h
+++ kklib/include/kklib/bytes.h
@@ -2,7 +2,7 @@
 #ifndef KK_BYTES_H
 #define KK_BYTES_H
 /*---------------------------------------------------------------------------
-  Copyright 2021 Microsoft Research, Daan Leijen.
+  Copyright 2021, Microsoft Research, Daan Leijen.
 
   This is free software; you can redistribute it and/or modify it under the
   terms of the Apache License, Version 2.0. A copy of the License can be
--- lib/std/text/regex-inline.c
+++ lib/std/text/regex-inline.c
@@ -1,7 +1,7 @@
 
 
 /*---------------------------------------------------------------------------
-  Copyright 2021 Microsoft Research, Daan Leijen.
+  Copyright 2021, Microsoft Research, Daan Leijen.
 
   This is free software; you can redistribute it and/or modify it under the
   terms of the Apache License, Version 2.0. A copy of the License can be
