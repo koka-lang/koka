77a3ff2e23d365db4e33fdd73468900761cdcde7
Daan Leijen <daan@microsoft.com>
2021-06-14 23:14:15 -0700

    fix copyright notice

--- src/Core/AnalysisResume.hs
+++ src/Core/AnalysisResume.hs
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------
--- Copyright 2017 Microsoft Research, Daan Leijen.
+-- Copyright 2017-2021, Microsoft Research, Daan Leijen.
 --
 -- This is free software; you can redistribute it and/or modify it under the
 -- terms of the Apache License, Version 2.0. A copy of the License can be
