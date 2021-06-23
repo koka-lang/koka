6fd29282f022b3f245274f6513c3b7c0574e3eee
Daan Leijen <daan@microsoft.com>
2021-06-23 12:44:07 -0700

    fix simplifying functions to values in Box

--- src/Backend/C/Box.hs
+++ src/Backend/C/Box.hs
@@ -63,8 +63,10 @@ boxDef :: Def -> Unique Def
 boxDef def
     = -- trace ("box def: " ++ show (defName def) ++ ": " ++ show (pretty (defType def)) ++ "\n" ++ show (prettyExpr Pretty.defaultEnv{Pretty.coreShowTypes=True} (defExpr def))) $
       do bexpr <- boxExpr (boxType (defType def)) (defExpr def)
-         expr  <- uniqueSimplify Pretty.defaultEnv True {- unsafe -} False {-ndebug-} 1 {-runs-} 6 {- duplicationMax -} bexpr
-         return def{ defExpr = expr }
+         let bdef = def{ defExpr = bexpr }
+         -- simplify the whole def to avoid simplifying away functions to values (e.g. `fun f(x){ g(x) } ~> val f = g`)
+         uniqueSimplify Pretty.defaultEnv True {- unsafe -} False {-ndebug-} 1 {-runs-} 6 {- duplicationMax -} bdef
+         
 
 -- add box/unbox such that the type of `expr` matches `BoxType`
 boxExpr :: BoxType -> Expr -> Unique Expr
