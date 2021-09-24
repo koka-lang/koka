703da936feb68220bef82f43ab2cf26caea7f99a
Daan Leijen <daan@microsoft.com>
2021-09-23 18:19:13 -0700

    fix spurious reuse for singleton datatypes

--- src/Backend/C/Parc.hs
+++ src/Backend/C/Parc.hs
@@ -227,7 +227,7 @@ optimizeGuard False ri dups rdrops
 optimizeGuard enabled ri dups rdrops
   = do shapes <- getShapeMap
        let mchildrenOf x = case M.lookup x shapes of
-                            Just (ShapeInfo mchildren _ _) -> mchildren
+                            Just (ShapeInfo (Just mchildren) _ _) | not (null mchildren) -> Just mchildren                            
                             _    -> Nothing
        let conNameOf x  = case M.lookup x shapes of
                             Just (ShapeInfo _ (Just (_,cname)) _) -> Just cname
