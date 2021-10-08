5cdcd32163c0ac9eac330318a5dd0b9624173e7e
Daan Leijen <daan@microsoft.com>
2021-10-08 14:17:52 -0700

    add Naoya to copyright

--- src/Core/OpenFloat.hs
+++ src/Core/OpenFloat.hs
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------
--- Copyright 2020 Microsoft Corporation, Daan Leijen, Ningning Xie
+-- Copyright 2020-2021, Microsoft Research, Daan Leijen, Naoya Furudono
 --
 -- This is free software; you can redistribute it and/or modify it under the
 -- terms of the Apache License, Version 2.0. A copy of the License can be
