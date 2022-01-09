9ceb539da8c5f8559643181f91532f9b12a11924
rubikscraft <contact@rubikscraft.nl>
2022-01-09 23:19:06 +0100

    add ability to restart koka language server

--- support/vscode/koka.language-koka/src/extension.ts
+++ support/vscode/koka.language-koka/src/extension.ts
@@ -1,30 +1,73 @@
 import * as vscode from 'vscode';
-import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions } from 'vscode-languageclient/node';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  RevealOutputChannelOn,
+  ServerOptions,
+} from 'vscode-languageclient/node';
 
 export function activate(context: vscode.ExtensionContext) {
-    const config = vscode.workspace.getConfiguration('koka');
+  const config = vscode.workspace.getConfiguration('koka');
 
-    if (config.get('languageServer.enabled')) {
-        const serverOptions: ServerOptions = {
-            command: config.get('languageServer.command'),
-            options: {
-                shell: true,
-                cwd: config.get('languageServer.cwd') || null
-            }
-        };
-        const clientOptions: LanguageClientOptions = {
-            documentSelector: [
-                { language: 'koka', scheme: 'file' }
-            ],
-            outputChannelName: 'Koka',
-            revealOutputChannelOn: RevealOutputChannelOn.Never
-        };
-        const client = new LanguageClient(
-            'Koka Language Client',
-            serverOptions,
-            clientOptions
-        );
+  // We can always create the client, as it does nothing as long as it is not started
+  const client = createClient(config);
 
-        context.subscriptions.push(client.start());
-    }
+  if (config.get('languageServer.enabled'))
+    context.subscriptions.push(client.start());
+
+  createCommands(context, config, client);
+}
+
+function createClient(config: vscode.WorkspaceConfiguration) {
+  const serverOptions: ServerOptions = {
+    command: config.get('languageServer.command'),
+    options: {
+      shell: true,
+      cwd: config.get('languageServer.cwd') || null,
+    },
+  };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ language: 'koka', scheme: 'file' }],
+    outputChannelName: 'Koka',
+    revealOutputChannelOn: RevealOutputChannelOn.Never,
+  };
+  const client = new LanguageClient(
+    'Koka Language Client',
+    serverOptions,
+    clientOptions,
+  );
+
+  return client;
+}
+
+function createCommands(
+  context: vscode.ExtensionContext,
+  config: vscode.WorkspaceConfiguration,
+  client: LanguageClient,
+) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('koka.restartLanguageServer', () => {
+      if (!config.get('languageServer.enabled'))
+        return vscode.window.showErrorMessage('Language server is not enabled');
+
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: 'Koka',
+          cancellable: false,
+        },
+        async (progress, token) => {
+          progress.report({ message: 'Restarting language server' });
+          await client.stop();
+          await client.start();
+          progress.report({
+            message: 'Language server restarted',
+            increment: 100,
+          });
+          // Wait 3 second to allow user to read message
+          await new Promise((resolve) => setTimeout(resolve, 3000));
+        },
+      );
+    }),
+  );
 }
