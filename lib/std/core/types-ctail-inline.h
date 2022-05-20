7a2649d3595f654468bb1880ad15f7c955e193fd
daan <daan@effp.org>
2022-05-19 18:08:23 -0700

    add affine effect optimization

--- lib/std/core/types-ctail-inline.h
+++ lib/std/core/types-ctail-inline.h
@@ -21,10 +21,10 @@ static inline kk_std_core_types__ctail kk_ctail_nil(kk_context_t* ctx) {
 }
 
 // apply a context to a child value
-static inline kk_box_t kk_ctail_resolve( kk_std_core_types__ctail acc, kk_box_t child, kk_context_t* ctx ) {
+static inline kk_box_t kk_ctail_resolve( kk_std_core_types__ctail acc, kk_box_t child, bool is_linear, kk_context_t* ctx ) {
   #if !defined(KK_CTAIL_NO_CONTEXT_PATH)
   // note: written like this for best codegen; be careful when rewriting.
-  if (kk_likely(acc.hole != NULL && kk_block_is_unique(kk_ptr_unbox(acc.res)))) {
+  if (kk_likely(acc.hole != NULL && (is_linear || kk_block_is_unique(kk_ptr_unbox(acc.res))))) {
     *(acc.hole) = child;   // in-place update the hole with the child
     return acc.res;      
   }
@@ -47,8 +47,8 @@ static inline kk_box_t kk_ctail_resolve( kk_std_core_types__ctail acc, kk_box_t 
 }
 
 // compose a context to a new one
-static inline kk_std_core_types__ctail kk_ctail_link( kk_std_core_types__ctail acc, kk_box_t child, kk_box_t* field, kk_context_t* ctx  ) {
-  return kk_std_core_types__new_CTail( kk_ctail_resolve(acc,child,ctx), field, ctx );
+static inline kk_std_core_types__ctail kk_ctail_link( kk_std_core_types__ctail acc, kk_box_t child, kk_box_t* field, bool is_linear, kk_context_t* ctx  ) {
+  return kk_std_core_types__new_CTail( kk_ctail_resolve(acc,child,is_linear,ctx), field, ctx );
 }
 
 
