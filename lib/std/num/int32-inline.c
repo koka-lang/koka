c6fecd3ffc8f05f4a118dd76036b1595be9b8995
Daan <daanl@outlook.com>
2024-03-22 07:32:55 -0700

    32-bit clmul

--- lib/std/num/int32-inline.c
+++ lib/std/num/int32-inline.c
@@ -17,3 +17,9 @@ static kk_std_core_types__tuple2 kk_imul32x_wide( int32_t x, int32_t y, kk_conte
   uint32_t lo = kk_imul32_wide(x, y, &hi);
   return kk_std_core_types__new_Tuple2( kk_int32_box((int32_t)lo,ctx), kk_int32_box(hi,ctx), ctx );
 }
+
+static kk_std_core_types__tuple2 kk_clmul32x_wide( int32_t x, int32_t y, kk_context_t* ctx ) {
+  uint32_t hi;
+  uint32_t lo = kk_clmul32_wide((uint32_t)x, (uint32_t)y, &hi);
+  return kk_std_core_types__new_Tuple2( kk_int32_box((int32_t)hi,ctx), kk_int32_box((int32_t)lo,ctx), ctx );
+}
