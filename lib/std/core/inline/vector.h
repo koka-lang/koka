eae154979c3c279c4f1e4196ca92cdc766692223
Tim Whiting <tim@whitings.org>
2024-06-08 14:47:43 -0600

    some updates to vectors to allow for reuse in unique mapping

--- lib/std/core/inline/vector.h
+++ lib/std/core/inline/vector.h
@@ -16,12 +16,11 @@
 kk_datatype_t  kk_vector_to_list(kk_vector_t v, kk_datatype_t tail, kk_context_t* ctx);
 kk_vector_t    kk_list_to_vector(kk_datatype_t xs, kk_context_t* ctx);
 
-static inline kk_unit_t kk_vector_unsafe_assign( kk_vector_t v, kk_ssize_t i, kk_box_t x, kk_context_t* ctx  ) {
+static inline kk_unit_t kk_vector_unsafe_assign_borrow( kk_vector_t v, kk_ssize_t i, kk_box_t x, kk_context_t* ctx  ) {
   kk_ssize_t len;
   kk_box_t* p = kk_vector_buf_borrow(v,&len,ctx);
   kk_assert(i < len);
   p[i] = x;
-  kk_vector_drop(v,ctx); // TODO: use borrowing
   return kk_Unit;
 }
 
@@ -29,4 +28,12 @@ static inline kk_box_t kk_vector_at_int_borrow( kk_vector_t v, kk_integer_t n, k
   // TODO: check bounds
   kk_box_t b = kk_vector_at_borrow(v,kk_integer_clamp_ssize_t_borrow(n,ctx),ctx);
   return b;
+}
+
+static inline kk_box_t kk_vector_unsafe_extract_borrow(const kk_vector_t v, kk_ssize_t i, kk_context_t* ctx) {
+  kk_assert(i < kk_vector_len_borrow(v,ctx));
+  kk_box_t* p = kk_vector_buf_borrow(v, NULL, ctx);
+  kk_box_t res = p[i];
+  p[i] = kk_box_null();
+  return res;
 }
