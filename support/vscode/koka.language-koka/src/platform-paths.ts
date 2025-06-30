d14c70e89cbd34535cc85e84231efc08c4710bc6
Tim Whiting <tim@whitings.org>
2025-06-30 16:03:59 -0600

    add linux-arm64

--- support/vscode/koka.language-koka/src/platform-paths.ts
+++ support/vscode/koka.language-koka/src/platform-paths.ts
@@ -4,7 +4,7 @@ import * as os from "os"
 export const home = os.homedir();
 export const kokaExeName = (os.platform() === "win32" ? "koka.exe" : "koka")
 export const defaultShell = (os.platform() === "win32" ? "C:\\Windows\\System32\\cmd.exe" : null)
-export const binaryPlatforms = ["windows-x64", "macos-arm64", "macos-x64", "linux-x64"]
+export const binaryPlatforms = ["windows-x64", "macos-arm64", "macos-x64", "linux-x64", "linux-arm64"]
 export const platform = osGetPlatform()
 export const arch = os.arch()
 export const targetPlatform = `${platform}-${arch}`
