d6202c030df80495d52b58658e974619204d80af
daanx <daan@effp.org>
2026-03-23 19:18:18 -0700

    update tests for new result type

--- lib/std/core/inline/exn.c
+++ lib/std/core/inline/exn.c
@@ -1,6 +1,6 @@
 
 /*---------------------------------------------------------------------------
-  Copyright 2020-2024, Microsoft Research, Daan Leijen.
+  Copyright 2020-2026, Microsoft Research, Daan Leijen.
 
   This is free software; you can redistribute it and/or modify it under the
   terms of the Apache License, Version 2.0. A copy of the License can be
